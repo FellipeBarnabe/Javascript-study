b144e689c0bb6a0daa253d11497740b205412f7b
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-08-14 10:50:02 -0300

    iniciando estudo javascprit variavel const

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,36 @@
-console.log("Meu primeiro programa");
+/* console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2; 
+
+"const" é uma palavra reservada do JavaScript usada para declarar uma constante,
+ ou seja, um identificador cujo valor não pode ser reatribuído. "idadeSomada" é o nome (identificador) dessa constante.
+"idade + 2" é uma expressão aritmética que pega o valor da variável "idade" e soma 2.
+O resultado dessa expressão é atribuído à constante "idadeSomada".
+Se usarmos console.log(idadeSomada), será exibido o valor da idade original acrescido de 2.
+console.log(idadeSomada); 
+*/ 
+
+
+console.log("Meu primeiro programa: trabalhando com variaveis");
+
+const idade = 35;
+
+console.log(idade+2)
+
+
+
+console.log("Minha idade daqui a dois anos e minha idade atual respectivamente é:",idadeSomada,"e minha idade atual é:", idade)
+// concatenação, você pode usar uma "" string junto com virgula para adicionaroutras variaveis e juntar. 
+
+
+const nome = "Lais";
+ console.log("nome"); /* sempre impresso no terminal "nome" , 
+                     pois não está sendo chamada a variavel constante nome que recebe o valor em string "lais". */

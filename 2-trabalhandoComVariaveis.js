709fb13389cd977f95bb697b0ca5853b486983b1
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-08-16 21:26:49 -0300

    praticando javascript

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -1,3 +1,16 @@
-const nome = "fellipe"; 
+const atletaDietaDiaria = 130;
+const cafeDaManha = 30;
+const almoço = 40;
+const jantar =30;
+var whey =30;
 
-console.log(nome);
+function consumoCaloria (){
+    if(cafeDaManha + almoço + jantar + whey < atletaDietaDiaria)
+        return "consumo insuficiente";
+    if(cafeDaManha + almoço + jantar + whey === atletaDietaDiaria)
+        return "consumo ideal"
+    if(cafeDaManha + almoço + jantar + whey > atletaDietaDiaria)
+        return "consumo excessivo"
+}
+
+console.log(consumoCaloria())
